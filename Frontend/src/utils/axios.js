d57eecb54f66389ecb6076f46cf3f08b80b178e5
saravanan510 <saravanan5101998@gmail.com>
2025-05-24 17:48:47 +0530

    dotenv

--- Frontend/src/utils/axios.js
+++ Frontend/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const Axios = axios.create({
-  baseURL: process.env.BACKEND_URL,
+  baseURL: import.meta.env.BACKEND_URL || "http://localhost:5000",
   // baseURL: "https://carehub-backend-l7gv.onrender.com/",
 });
 
