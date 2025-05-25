ba3c2689eb0604db88899c24a36810ebce219cc5
saravanan510 <saravanan5101998@gmail.com>
2025-05-25 12:53:44 +0530

    url

--- Frontend/src/utils/axios.js
+++ Frontend/src/utils/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const Axios = axios.create({
   // baseURL: "http://carehubuae.com/api",
-  baseURL: "http://localhost:5000",
+  // baseURL: "http://localhost:5000",
+  baseURL: "http://147.93.110.142:5000",
 });
 
 export default Axios;
