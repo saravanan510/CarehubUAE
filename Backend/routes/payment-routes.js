3f012ee4bee55f1a717ad14ef18b1ea3cb2d4292
saravanan510 <saravanan5101998@gmail.com>
2025-05-30 18:42:04 +0530

    handle get

--- Backend/routes/payment-routes.js
+++ Backend/routes/payment-routes.js
@@ -1,4 +1,3 @@
-// routes/payment.js
 const express = require("express");
 const router = express.Router();
 const paymentCtrl = require("../app/controllers/paymentCtrl");
@@ -6,5 +5,17 @@ const paymentCtrl = require("../app/controllers/paymentCtrl");
 router.post("/initiate", paymentCtrl.initiatePayment);
 router.post("/success", paymentCtrl.paymentResponse);
 router.post("/cancel", paymentCtrl.paymentResponse);
+
+router.get("/success", (req, res) => {
+  res.redirect(
+    `${process.env.YOUR_REACT_APP_URL}/paymentStatus?status=success`
+  );
+});
+
+router.get("/cancel", (req, res) => {
+  res.redirect(
+    `${process.env.YOUR_REACT_APP_URL}/paymentStatus?status=cancelled`
+  );
+});
 
 module.exports = router;
