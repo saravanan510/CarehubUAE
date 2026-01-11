11cd9c0abbb2a0b8e55908f2d1a6c0bff293f48f
saravanan510 <saravanan5101998@gmail.com>
2026-01-11 14:46:34 +0530

    app fix

--- Frontend/src/app.tsx
+++ Frontend/src/app.tsx
@@ -54,7 +54,7 @@ let router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: "/private-nursing-dubai",
+        path: "/private-nursing",
         element: <PrivateNursing />,
       },
       {
